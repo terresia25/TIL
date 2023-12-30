392a2f2a90361e46e1234d05985a53a8b8476cb6
Terresia <terresia25@gmail.com>
2023-12-30 17:20:12 +0900

    10808

---
+++ subaksu/10808.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+string str;
+int cnt[26];
+
+int main() {
+	cin >> str;
+
+	for (char a : str) {
+		cnt[a - 'a']++;
+	}
+	for (int i = 0; i < 26; i++) {
+		cout << cnt[i] << " ";
+	}
+
+	return 0;
+}
